a8e39b27e5af29e3587008ff22e53cc7f7f94cd1
Abhishek <abhipgore@gmail.com>
2018-10-24 20:27:13 +0530

    Fixed contact.html and added team.html

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.modal').modal();
-});
+  	$('.carousel.carousel-slider').carousel({
+    fullWidth: true});
+  });
